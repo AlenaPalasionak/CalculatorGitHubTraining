899690ccdab69ef4ee61d8b4f0cc27ed5e80bd21
Lena Palasionak <19lenka87@tut.by>
2022-01-12 23:16:00 +0300

    Formatted.

--- src/main/java/CalculatorImpl.java
+++ src/main/java/CalculatorImpl.java
@@ -4,8 +4,6 @@ import org.apache.commons.logging.LogFactory;
 public class CalculatorImpl implements Calculator {
     private static final Log LOG = LogFactory.getLog(CalculatorImpl.class);
 
-
-
     @Override
     public int add(int value1, int value2) {
         LOG.debug("Value1 = " + value1 + ", Value2 = " + value2);
@@ -20,20 +18,20 @@ public class CalculatorImpl implements Calculator {
     }
 
     @Override
-    public int divide(int value1, int value2)  throws DivisionException {
+    public int divide(int value1, int value2) throws DivisionException {
         LOG.debug("Value1 = " + value1 + ", Value2 = " + value2);
-        if(value2 == 0) {
+        if (value2 == 0) {
             throw new DivisionException("value2 == 0");
-        }else if (value1 % value2 != 0){
+        } else if (value1 % value2 != 0) {
             throw new DivisionException("result is not Integer");
         }
 
-        return value1/value2;
+        return value1 / value2;
     }
 
     @Override
     public int multiply(int value1, int value2) {
         LOG.debug("Value1 = " + value1 + ", Value2 = " + value2);
-        return value1*value2;
+        return value1 * value2;
     }
 }
